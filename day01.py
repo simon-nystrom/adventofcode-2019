9e25641fce20487fd0698f21468d215db45daed4
Simon Nyström <simon.nystrom90@gmail.com>
2019-12-02 11:14:08 +0100

    Day 1 complete

---
+++ day01.py
@@ -0,0 +1,25 @@
+from utils.timing import timing
+
+file = open("day01.txt")
+puzzle_input = [int(x) for x in file.read().split('\n')]
+file.close()
+
+def calculate_fuel(mass):
+    return mass // 3 - 2;
+
+def calculate_fuel2(mass, current_fuel = 0):
+    req_fuel = calculate_fuel(mass)
+    if (req_fuel <= 0):
+        return current_fuel
+    return calculate_fuel2(req_fuel, current_fuel + req_fuel)
+
+@timing
+def part1(puzzle_input):
+    return reduce(lambda prev, mass: prev + calculate_fuel(mass), puzzle_input, 0)
+
+@timing
+def part2(puzzle_input):
+    return reduce(lambda prev, mass: prev + calculate_fuel2(mass), puzzle_input, 0)
+
+part1(puzzle_input)
+part2(puzzle_input)
