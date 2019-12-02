1d50d66caa1df3b8913ac80d08c1b7294e18a925
Simon Nyström <simon.nystrom90@gmail.com>
2019-12-02 14:00:38 +0100

    formatting

--- utils/printing.py
+++ utils/printing.py
@@ -5,7 +5,7 @@ def printed(f):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        print('{:s}: {} - took {:.3f} ms'.format(f.__name__, ret, (time2-time1)*1000.0))
+        print('{:s}: {} - took {:.3f} ms'.format(f.__name__, ret, (time2 - time1) * 1000.0))
 
         return ret
     return wrap
